490ba918347e950afe7cb3ec0e06c0ee7a40d058
ADRIAN ZGARBIK <adrianzgarbik@gmail.com>
2023-03-22 20:50:26 -0300

    mi primer commit de clase 2

---
+++ app.js
@@ -0,0 +1,28 @@
+//ej 1
+var auto = "corolla"; 
+console.log(auto)
+//ej 6
+var tipo = typeof auto;
+console.log(tipo)
+//ej 2
+var auto = 308;
+console.log(auto);
+//ej 6
+var tipo = typeof auto;
+console.log(tipo)
+//ej 3
+var auto = "C4"; 
+console.log(auto)
+//ej 6
+var tipo = typeof auto;
+console.log(tipo)
+
+//ej 4
+var nombre = "Adrian";
+var apellido ="Zgarbik";
+console.log (nombre , apellido)
+
+//ej 5
+var fruta = "naranja";
+var tipoDeLaVariableFruta = typeof(fruta)
+console.log(tipoDeLaVariableFruta)
